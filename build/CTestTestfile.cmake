87522bf3db515e8851052d93cbce4586f5eff2e1
kris <krisi2001@gmail.com>
2019-09-02 04:05:55 +0300

    added gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kris/C++/Hardcoded self-destruction
-# Build directory: /home/kris/C++/Hardcoded self-destruction/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
